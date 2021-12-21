a5b2210c59b207f6c00271488df5eb6ddfe7d0aa
ainyrbikmurzin <96476318+ainyrbikmurzin@users.noreply.github.com>
2021-12-21 15:18:16 +0300

    Add files via upload

---
+++ 1 zadanie/WinAnim/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAnim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAW_BLEND_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            WinApiClass.AnimateWindow(this, 3000, WinApiClass.AnimateWindowFlags.AW_ACTIVATE | WinApiClass.AnimateWindowFlags.AW_BLEND);
+            this.btnAW_BLEND.Invalidate();
+            this.btnHOR_AW_SLIDE.Invalidate();
+            this.btnCenter_AW_SLIDE.Invalidate();
+        }
+
+        private void btnHOR_AW_SLIDE_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            WinApiClass.AnimateWindow(this, 3000, WinApiClass.AnimateWindowFlags.AW_HOR_ACTIVATE | WinApiClass.AnimateWindowFlags.AW_SLIDE);
+            this.btnAW_BLEND.Invalidate();
+            this.btnHOR_AW_SLIDE.Invalidate();
+            this.btnCenter_AW_SLIDE.Invalidate();
+        }
+
+        private void btnCenter_AW_SLIDE_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            WinApiClass.AnimateWindow(this, 3000, WinApiClass.AnimateWindowFlags.AW_CENTER | WinApiClass.AnimateWindowFlags.AW_SLIDE);
+            this.btnAW_BLEND.Invalidate();
+            this.btnHOR_AW_SLIDE.Invalidate();
+            this.btnCenter_AW_SLIDE.Invalidate();
+        }
+    }
+}
